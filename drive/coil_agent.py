206fafc3efffa0751b4092f049a4dc61bd80a153
felipecode <felipe.alcm@gmail.com>
2018-06-07 08:33:05 +0200

    coil Agent

--- drive/coil_agent.py
+++ drive/coil_agent.py
@@ -40,37 +40,13 @@ except ImportError:
 number_of_seg_classes = 5
 classes_join = {0: 2, 1: 2, 2: 2, 3: 2, 5: 2, 12: 2, 9: 2, 11: 2, 4: 0, 10: 1, 8: 3, 6: 3, 7: 4}
 
-# TODO: implement this as a torch operation.
+# TODO: implement this as a torch operation. maybe...
 def join_classes(labels_image):
     compressed_labels_image = np.copy(labels_image)
     for key, value in classes_join.iteritems():
         compressed_labels_image[np.where(labels_image == key)] = value
 
     return compressed_labels_image
-
-
-"""
-
-
-def restore_session(sess, saver, models_path, checkpoint_number):
-    ckpt = 0
-    if not os.path.exists(models_path):
-        os.mkdir(models_path)
-
-    ckpt = tf.train.get_checkpoint_state(models_path)
-    if checkpoint_number != None:
-        ckpt.model_checkpoint_path = os.path.join(models_path,
-                                                  'model.ckpt-' + str(checkpoint_number))
-    if ckpt:
-        print 'Restoring from ', ckpt.model_checkpoint_path
-        saver.restore(sess, ckpt.model_checkpoint_path)
-    else:
-        ckpt = 0
-
-    return ckpt
-"""
-
-""" Initializing Session as variables that control the session """
 
 
 
@@ -131,7 +107,10 @@ class CoILAgent(Agent):
         for name, size in g_conf.SENSORS.items():
 
             sensor = sensors[name].data[g_conf.IMAGE_CUT[0]:g_conf.IMAGE_CUT[1], ...]
+
+
             if  sensors[name].type == 'SemanticSegmentation':
+                # For now we have just for RGB images and semantic segmentation.
 
 
                 # TODO: the camera name has to be sincronized with what is in the experiment...
@@ -139,23 +118,29 @@ class CoILAgent(Agent):
 
                 sensor = sensor[:, :, np.newaxis]
 
-                image_transform = transforms.Compose([transforms.ToTensor(),
-                                   transforms.Resize((size[1], size[2]), interpolation=Image.NEAREST),
-                                   iag.ToGPU(), iag.Multiply((1 / (number_of_seg_classes - 1)))])
+
+                sensor = transforms.resize(sensor, (size[1], size[2]), interpolation=Image.NEAREST)
+                sensor = sensor * (1 / (number_of_seg_classes - 1))
+
+                sensor = torch.from_numpy(sensor).type(torch.FloatTensor).cuda()
+
+                # OBS: Is using image transform better ?
+
             else:
 
-                image_transform = transforms.Compose([transforms.ToPILImage(),
-                                   transforms.Resize((size[1], size[2])),
-                                   transforms.ToTensor(), transforms.Normalize((0, 0 ,0), (255, 255, 255)),
-                                   iag.ToGPU()])
+                sensor = transforms.resize(sensor, (size[1], size[2]))
+
+                sensor = torch.from_numpy(sensor).type(torch.FloatTensor).cuda()
+
+
 
 
             sensor = np.swapaxes(sensor, 0, 1)
 
-            sensor = np.flip(sensor.transpose((2, 0, 1)), axis=0)
+            sensor = sensor.transpose((2, 0, 1))
 
             if iteration == 0:
-                image_input = image_transform(sensor)
+                image_input = sensor
             else:
                 image_input = torch.cat((image_input, sensor), 0)
 
@@ -167,7 +152,7 @@ class CoILAgent(Agent):
         return image_input
 
 
-    def _process_model_outputs(self,outputs, speed):
+    def _process_model_outputs(self, outputs, speed):
         """
          A bit of heuristics in the control, to eventually make car faster, for instance.
         Returns:
@@ -179,110 +164,10 @@ class CoILAgent(Agent):
 
         if throttle > brake:
             brake = 0.0
-        else:
-            throttle = throttle * 2
-        if speed > 35.0 and brake == 0.0:
-            throttle = 0.0
+        #else:
+        #    throttle = throttle * 2
+        #if speed > 35.0 and brake == 0.0:
+        #    throttle = 0.0
 
 
         return steer, throttle, brake
-
-
-    """
-
-    def compute_action(self, sensors, speed, direction):
-
-        capture_time = time.time()
-
-
-        sensor_pack = []
-
-
-
-        for i in range(len(sensors)):
-
-            sensor = sensors[i]
-
-            sensor = sensor[g_conf.IMAGE_CUT[0]:g_conf.IMAGE_CUT[1], :]
-
-            if g_conf.param.SENSORS.keys()[i] == 'rgb':
-
-                sensor = scipy.misc.imresize(sensor, [self._config.sensors_size[i][0],
-                                                      self._config.sensors_size[i][1]])
-
-
-            elif g_conf.param.SENSORS.keys()[i] == 'labels':
-
-                sensor = scipy.misc.imresize(sensor, [self._config.sensors_size[i][0],
-                                                      self._config.sensors_size[i][1]],
-                                             interp='nearest')
-
-                sensor = join_classes(sensor) * int(255 / (number_of_seg_classes - 1))
-
-                sensor = sensor[:, :, np.newaxis]
-
-            sensor_pack.append(sensor)
-
-        if len(sensor_pack) > 1:
-
-            image_input = np.concatenate((sensor_pack[0], sensor_pack[1]), axis=2)
-
-        else:
-            image_input = sensor_pack[0]
-
-        image_input = image_input.astype(np.float32)
-        image_input = np.multiply(image_input, 1.0 / 255.0)
-        
-
-        image_input = sensors[0]
-
-        image_input = image_input.astype(np.float32)
-        image_input = np.multiply(image_input, 1.0 / 255.0)
-        # TODO: This will of course depend on the model , if it is based on sequences there are
-        # TODO: different requirements
-        #tensor = self.model(image_input)
-        outputs = self.model.forward_branch(image_input, speed, direction)
-
-
-
-        return control  # ,machine_output_functions.get_intermediate_rep(image_input,speed,self._config,self._sess,self._train_manager)
-
-    """
-    """
-    def compute_perception_activations(self, sensor, speed):
-
-        sensor = sensor[self._image_cut[0]:self._image_cut[1], :, :]
-
-        sensor = scipy.misc.imresize(sensor, [self._config.network_input_size[0],
-                                              self._config.network_input_size[1]])
-
-        image_input = sensor.astype(np.float32)
-
-        # print future_image
-
-        # print "2"
-        image_input = np.multiply(image_input, 1.0 / 255.0)
-
-        vbp_image = machine_output_functions.vbp(image_input, speed, self._config, self._sess,
-                                                 self._train_manager)
-
-        min_max_scaler = preprocessing.MinMaxScaler()
-        vbp_image = min_max_scaler.fit_transform(np.squeeze(vbp_image))
-
-        # print vbp_image
-        # print vbp_image
-        # print grayscale_colormap(np.squeeze(vbp_image),'jet')
-
-        vbp_image_3 = np.copy(image_input)
-        vbp_image_3[:, :, 0] = vbp_image
-        vbp_image_3[:, :, 1] = vbp_image
-        vbp_image_3[:, :, 2] = vbp_image
-        # print vbp_image
-
-        return 0.4 * grayscale_colormap(np.squeeze(vbp_image), 'inferno') + 0.6 * image_input
-
-    def get_waypoints(self):
-
-        wp1, wp2 = self._agent.get_active_wps()
-        return [wp1, wp2]
-    """
