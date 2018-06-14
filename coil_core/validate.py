c507ddc09d06dbc75143aeb83682c68956be69e2
felipecode <felipe.alcm@gmail.com>
2018-06-14 12:34:01 +0200

    validation fixes

--- coil_core/validate.py
+++ coil_core/validate.py
@@ -4,6 +4,7 @@ import sys
 
 import numpy as np
 import torch
+import traceback
 import torch.optim as optim
 import random
 
@@ -74,106 +75,117 @@ def execute(gpu, exp_batch, exp_alias, dataset_name, suppress_output):
     best_loss_iter = 0
     best_error_iter = 0
 
-    while not maximun_checkpoint_reach(latest, g_conf.TEST_SCHEDULE):
+    
+    try:
+        while not maximun_checkpoint_reach(latest, g_conf.TEST_SCHEDULE):
 
-        if is_next_checkpoint_ready(g_conf.TEST_SCHEDULE):
+            if is_next_checkpoint_ready(g_conf.TEST_SCHEDULE):
 
-            latest = get_next_checkpoint(g_conf.TEST_SCHEDULE)
+                latest = get_next_checkpoint(g_conf.TEST_SCHEDULE)
 
-            checkpoint = torch.load(os.path.join('_logs', exp_batch, exp_alias
-                                    , 'checkpoints', str(latest) + '.pth'))
-            checkpoint_iteration = checkpoint['iteration']
-            print ("Validation loaded ", checkpoint_iteration)
-
-
-
-            accumulated_loss = 0
-            accumulated_error = 0
-            iteration_on_checkpoint = 0
-            for data in data_loader:
-
-                input_data, float_data = data
-                control_position = np.where(dataset.meta_data[:, 0] == b'control')[0][0]
-                speed_position = np.where(dataset.meta_data[:, 0] == b'speed_module')[0][0]
-                print (torch.squeeze(input_data['rgb']).shape)
-
-                print (control_position)
-                print (speed_position)
-                # Obs : Maybe we could also check for other branches ??
-
-                output = model.forward_branch(torch.squeeze(input_data['rgb']).cuda(),
-                                              float_data[:, speed_position, :].cuda(),
-                                              float_data[:, control_position, :].cuda())
+                checkpoint = torch.load(os.path.join('_logs', exp_batch, exp_alias
+                                        , 'checkpoints', str(latest) + '.pth'))
+                checkpoint_iteration = checkpoint['iteration']
+                print ("Validation loaded ", checkpoint_iteration)
 
 
 
+                accumulated_loss = 0
+                accumulated_error = 0
+                iteration_on_checkpoint = 0
+                for data in data_loader:
 
-                for i in range(input_data['rgb'].shape[0]):
+                    input_data, float_data = data
+                    control_position = np.where(dataset.meta_data[:, 0] == b'control')[0][0]
+                    speed_position = np.where(dataset.meta_data[:, 0] == b'speed_module')[0][0]
+                    print (torch.squeeze(input_data['rgb']).shape)
 
-                    coil_logger.write_on_csv(checkpoint_iteration, [output[i][0],
-                                                                    output[i][1],
-                                                                    output[i][2]])
+                    print (control_position)
+                    print (speed_position)
+                    # Obs : Maybe we could also check for other branches ??
 
-
-                # TODO: Change this a functional standard using the loss functions.
-
-                loss = torch.mean((output - dataset.extract_targets(float_data).cuda())**2).data.tolist()
-                mean_error = torch.mean(torch.abs(output - dataset.extract_targets(float_data).cuda())).data.tolist()
-                accumulated_error += mean_error
-                accumulated_loss += loss
-                error = torch.abs(output - dataset.extract_targets(float_data).cuda())
+                    output = model.forward_branch(torch.squeeze(input_data['rgb']).cuda(),
+                                                  float_data[:, speed_position, :].cuda(),
+                                                  float_data[:, control_position, :].cuda())
 
 
-                # Log a random position
-                position = random.randint(0, len(float_data) - 1)
-                #print (output[position].data.tolist())
+
+
+                    for i in range(input_data['rgb'].shape[0]):
+
+                        coil_logger.write_on_csv(checkpoint_iteration, [output[i][0],
+                                                                        output[i][1],
+                                                                        output[i][2]])
+
+
+                    # TODO: Change this a functional standard using the loss functions.
+
+                    loss = torch.mean((output - dataset.extract_targets(float_data).cuda())**2).data.tolist()
+                    mean_error = torch.mean(torch.abs(output - dataset.extract_targets(float_data).cuda())).data.tolist()
+                    accumulated_error += mean_error
+                    accumulated_loss += loss
+                    error = torch.abs(output - dataset.extract_targets(float_data).cuda())
+
+
+                    # Log a random position
+                    position = random.randint(0, len(float_data) - 1)
+                    #print (output[position].data.tolist())
+                    coil_logger.add_message('Iterating',
+                         {'Checkpoint': latest,
+                          'Iteration': (str(iteration_on_checkpoint*120)+'/'+str(len(dataset))),
+                          'MeanError': mean_error,
+                          'Loss': loss,
+                          'Output': output[position].data.tolist(),
+                          'GroundTruth': dataset.extract_targets(float_data)[position].data.tolist(),
+                          'Error': error[position].data.tolist(),
+                          'Inputs': dataset.extract_inputs(float_data)[position].data.tolist()},
+                          latest)
+                    iteration_on_checkpoint += 1
+
+                checkpoint_average_loss = accumulated_loss/len(data_loader)
+                checkpoint_average_error = accumulated_error/len(data_loader)
+                coil_logger.add_scalar('Loss', checkpoint_average_loss, latest)
+                coil_logger.add_scalar('Error', checkpoint_average_error, latest)
+
+                if checkpoint_average_loss < best_loss:
+                    best_loss = checkpoint_average_loss
+                    best_loss_iter = latest
+
+                if checkpoint_average_error < best_loss:
+                    best_error = checkpoint_average_error
+                    best_error_iter = latest
+
                 coil_logger.add_message('Iterating',
-                     {'Checkpoint': latest,
-                      'Iteration': (str(iteration_on_checkpoint*120)+'/'+str(len(dataset))),
-                      'MeanError': mean_error,
-                      'Loss': loss,
-                      'Output': output[position].data.tolist(),
-                      'GroundTruth': dataset.extract_targets(float_data)[position].data.tolist(),
-                      'Error': error[position].data.tolist(),
-                      'Inputs': dataset.extract_inputs(float_data)[position].data.tolist()},
-                      latest)
-                iteration_on_checkpoint += 1
 
-            checkpoint_average_loss = accumulated_loss/len(data_loader)
-            checkpoint_average_error = accumulated_error/len(data_loader)
-            coil_logger.add_scalar('Loss', checkpoint_average_loss, latest)
-            coil_logger.add_scalar('Error', checkpoint_average_error, latest)
+                     {'Summary':
+                         {
+                          'Error': checkpoint_average_error,
+                          'Loss': checkpoint_average_loss,
+                          'BestError': best_error,
+                          'BestLoss': best_loss,
+                          'BestLossCheckpoint': best_loss_iter,
+                          'BestErrorCheckpoint': best_error_iter
+                         },
 
-            if checkpoint_average_loss < best_loss:
-                best_loss = checkpoint_average_loss
-                best_loss_iter = latest
+                     'Checkpoint': latest},
+                                        latest)
 
-            if checkpoint_average_error < best_loss:
-                best_error = checkpoint_average_error
-                best_error_iter = latest
+            else:
+                time.sleep(1)
+                print ("Waiting for the next Validation")
 
-            coil_logger.add_message('Iterating',
-
-                 {'Summary':
-                     {
-                      'Error': checkpoint_average_error,
-                      'Loss': checkpoint_average_loss,
-                      'BestError': best_error,
-                      'BestLoss': best_loss,
-                      'BestLossCheckpoint': best_loss_iter,
-                      'BestErrorCheckpoint': best_error_iter
-                     },
-
-                 'Checkpoint': latest},
-                                    latest)
-
-        else:
-            time.sleep(1)
-            print ("Waiting for the next Validation")
-
-    coil_logger.add_message('Finished', {})
+        coil_logger.add_message('Finished', {})
 
 
-    # TODO: DO ALL THE AMAZING LOGGING HERE, as a way to very the status in paralell.
-    # THIS SHOULD BE AN INTERELY PARALLEL PROCESS
+        # TODO: DO ALL THE AMAZING LOGGING HERE, as a way to very the status in paralell.
+        # THIS SHOULD BE AN INTERELY PARALLEL PROCESS
+
+    except KeyboardInterrupt:
+        coil_logger.add_message('Error', {'Message': 'Killed By User'})
+
+    except:
+        traceback.print_exc()
+
+        coil_logger.add_message('Error', {'Message': 'Something Happened'})
+
 
