0ec9ad1924fb14b6bcc6b2e74fecb180872acd68
felipecode <felipe.alcm@gmail.com>
2018-06-15 14:21:33 +0200

    making add metadata to work always

--- tools/add_metadata.py
+++ tools/add_metadata.py
@@ -71,7 +71,11 @@ if __name__ == "__main__":
         dt = h5py.special_dtype(vlen=str)  # PY3
 
         #del f[metadata_targets]
-        metadata = data.create_dataset('metadata_targets', (35, 2, ), dtype=dt)
+        try:
+            metadata = data.create_dataset('metadata_targets', (35, 2, ), dtype=dt)
+        except:
+            del data['metadata_targets']
+            metadata = data.create_dataset('metadata_targets', (35, 2,), dtype=dt)
 
         metadata[0,0], metadata[0,1] = 'steer', 'float'
         metadata[1,0], metadata[1,1] = 'throttle', 'float'
