d80c0413ba6c4d7dda8e5ee0904f23d425aeef11
felipecode <felipe.alcm@gmail.com>
2018-06-22 19:01:31 +0200

    train

--- coil_core/train.py
+++ coil_core/train.py
@@ -134,8 +134,7 @@ def execute(gpu, exp_batch, exp_alias, suppress_output=True):
 
         #TODO: test experiment continuation. Is the data sampler going to continue were it started.. ?
         capture_time = time.time()
-        data = next(iter(data_loader))
-        for _ in range(iteration, g_conf.NUMBER_ITERATIONS):
+        for data in data_loader:
 
 
             input_data, float_data = data
