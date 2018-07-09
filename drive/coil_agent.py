cb62a60c2b5bc0238ab5567b58bd4779832f95fc
Felipe Codevilla <felipe.alcm@gmail.com>
2018-07-09 15:39:19 -0700

    adding exps and a debug folder, agent now performs on km/h

--- drive/coil_agent.py
+++ drive/coil_agent.py
@@ -68,7 +68,7 @@ class CoILAgent(Agent):
 
         # control_agent = self._agent.run_step(measurements, None, target)
 
-        norm_speed = measurements.player_measurements.forward_speed / g_conf.SPEED_FACTOR
+        norm_speed = (measurements.player_measurements.forward_speed * 3.6) / g_conf.SPEED_FACTOR
         norm_speed = torch.cuda.FloatTensor([norm_speed]).unsqueeze(0)
 
         directions_tensor = torch.cuda.LongTensor([directions])
