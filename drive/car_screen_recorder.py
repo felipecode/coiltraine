61b08f13ccd2a930066ac10c5c69f627b04c9051
Felipe Codevilla <felipe.alcm@gmail.com>
2018-07-22 18:46:29 -0700

    car screen recorder

--- drive/car_screen_recorder.py
+++ drive/car_screen_recorder.py
@@ -135,6 +135,7 @@ class CarScreenRecorder(object):
     def record_frame(self, sensor_data, control, direction,
                      measurements, screen_position=[0, 0]):
         # direction, speed, screen_position=[0, 0], draw_wheel=False):
+        sensor_data.setflags(write=1)
         speed = measurements.player_measurements.forward_speed
         #start_to_print = time.time()
         steer = control.steer
