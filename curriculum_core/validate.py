1097c76865d1c131f6844eab6a0b2876c70a1b5d
eder.santana <eder.santana@tri.global>
2018-08-27 22:37:36 +0000

    curriculum_core/{train, validate} use_preload equal true

--- curriculum_core/validate.py
+++ curriculum_core/validate.py
@@ -39,7 +39,7 @@ def execute(checkpoint, output_file, gpu):
         #Define the dataset. This structure is has the __get_item__ redefined in a way
         #that you can access the HDFILES positions from the root directory as a in a vector.
         # full_dataset = os.path.join(os.environ["COIL_DATASET_PATH"], 'Town02W14Noise')
-        full_dataset = os.path.join(os.environ["COIL_DATASET_PATH"], 'CARLA100_2/CARLA100_2')
+        full_dataset = os.path.join(os.environ["COIL_DATASET_PATH"], 'CARLA100_2/CARLA100_2', use_preload=True)
         augmenter = Augmenter(None)
         dataset = CoILDataset(full_dataset, transform=augmenter)
         data_loader = torch.utils.data.DataLoader(dataset, batch_size=120,
