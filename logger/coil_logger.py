2c882e7078e5b4506cf4b74672458cbc42864d05
Rares <rares.ambrus@tri.global>
2018-10-01 22:57:18 +0000

    new_images

--- logger/coil_logger.py
+++ logger/coil_logger.py
@@ -193,7 +193,7 @@ def add_image(tag, images, iteration=None):
                 cmap = plt.get_cmap()
                 for i in range(images.shape[0]):
                     new_images.append(cmap(images[i])[None, :, :, :3])
-                new_images = np.concatenate(new_images, 0).transpose(0, 3, 1, 2)
+                images = np.concatenate(new_images, 0).transpose(0, 3, 1, 2)
                 
             print ("Converted")
             print (images.shape)
