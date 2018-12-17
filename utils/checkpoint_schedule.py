af90a7fdc540eb4f4ad833cd1217a574b06fc536
Felipe Codevilla <felipe.alcm@gmail.com>
2018-12-17 14:08:24 +0100

    checkpoint schedule

--- utils/checkpoint_schedule.py
+++ utils/checkpoint_schedule.py
@@ -103,12 +103,10 @@ def is_next_checkpoint_ready(checkpoint_schedule, control_filename=None):
         return False
 
 
-def get_next_checkpoint(checkpoint_schedule, filename):
+def get_next_checkpoint(checkpoint_schedule, filename=None):
     ltst_check = get_latest_evaluated_checkpoint(filename)
     if ltst_check is None:
         return checkpoint_schedule[0]
-
-
 
     if checkpoint_schedule.index(ltst_check) + 1 == len(checkpoint_schedule):
         raise RuntimeError("Not able to get next checkpoint, maximum checkpoint is reach")
@@ -116,45 +114,3 @@ def get_next_checkpoint(checkpoint_schedule, filename):
     return checkpoint_schedule[checkpoint_schedule.index(ltst_check) + 1]
 
 
-
-#
-# def next_check_point_ready():
-#     """
-#     Looks at every checkpoint file in the folder. And for each of
-#     then tries to find the one that matches EXACTLY with the one in the schedule
-#
-#     :return:
-#     """
-#
-#     checkpoint_files = sorted(os.listdir(self._config_input.models_path))
-#     for f in checkpoint_files:
-#
-#         match = re.search('model.ckpt-(\d+)', f)
-#         if match:
-#             checkpoint_number = match.group(1)
-#
-#             if int(checkpoint_number) == (self._checkpoint_schedule[self._current_checkpoint_number]):
-#                 self._checkpoint_number_to_test = str(self._checkpoint_schedule[self._current_checkpoint_number])
-#
-#                 return True
-#     logging.info('Checkpoint Not Found, Will wait for %d' % self._checkpoint_schedule[self._current_checkpoint_number] )
-#     return False
-#
-# def get_test_name():
-#
-#     return str(self._checkpoint_number_to_test)
-#
-# def finish_model():
-#     """
-#     Increment and go to the next model
-#
-#     :return None:
-#
-#     """
-#     self._current_checkpoint_number += 1
-
-
-def is_iteration_for_saving():
-
-
-    return True
