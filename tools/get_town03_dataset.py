e457477162988c3d886de4d9bd1b0e2308f10c74
Felipe Codevilla <felipe.alcm@gmail.com>
2019-01-30 14:45:50 +0100

    adding download tools for the dataset

---
+++ tools/get_town03_dataset.py
@@ -0,0 +1,32 @@
+import os
+import tarfile
+
+from .download_tools import download_file_from_google_drive
+
+
+
+if __name__ == "__main__":
+    try:
+        path = os.environ["COIL_DATASET_PATH"]
+    except KeyError as e:
+        print("")
+        print("COIL_DATASET_PATH env variable must be defined.")
+        print("")
+        raise e
+
+    # Download the datasets
+    file_id = '18XgqpR4Kxq14_oC9jJ-RHZS0aqscwxcD'
+    destination_pack = 'CoILTrainTown03.tar.gz'
+
+    print("Downloading the training dataset for Town03  (5GB total)")
+    download_file_from_google_drive(file_id, destination_pack)
+    destination_final = os.path.join("~/", os.environ["COIL_DATASET_PATH"])
+    if not os.path.exists(destination_final):
+        os.makedirs(destination_final)
+
+    print("Unpacking the dataset (takes a few minutes)")
+
+    tf = tarfile.open("CoILTrainTown03.tar.gz")
+    tf.extractall(destination_final)
+
+    os.remove("CoILTrainTown03.tar.gz")
