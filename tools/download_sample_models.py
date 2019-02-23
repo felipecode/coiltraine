ebc2982bd7d3f9c5a254bb84ece0b150de0367f6
Felipe Codevilla <felipe.alcm@gmail.com>
2019-02-23 07:35:43 -0500

    fix sample downloading

--- tools/download_sample_models.py
+++ tools/download_sample_models.py
@@ -4,7 +4,7 @@ import sys
 
 import tarfile
 
-PACKAGE_PARENT = '..'
+PACKAGE_PARENT = '.'
 SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
 sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
