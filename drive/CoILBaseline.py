6dac3d73f3b5dfc3c6cdd4a8a6c06518008a683e
Felipe Codevilla <felipe.alcm@gmail.com>
2019-02-26 12:53:21 -0500

    CoIL Baseline

--- drive/CoILBaseline.py
+++ drive/CoILBaseline.py
@@ -51,7 +51,7 @@ def distance_vehicle(waypoint, vehicle_position):
 
 class CoILBaseline(AutonomousAgent):
 
-    def setup(self):
+    def setup(self, path_to_config_file):
 
         config_file = "file.xml"
         yaml_conf, checkpoint_number = checkpoint_parse_configuration_file(config_file)
@@ -94,6 +94,22 @@ class CoILBaseline(AutonomousAgent):
                    ['sensor.other.gnss', {'x': 0.7, 'y': -0.4, 'z': 1.60},
                     'GPS']
                    ]
+        """
+        sensors = [{'type': 'sensor.camera.rgb', 'x':0.7, 'y':0.0, 'z':1.60, 'roll':0.0, 'pitch':0.0, 'yaw':0.0,
+                    'width':800, 'height': 600, 'fov':100, 'id': 'Center'},
+                   {'type': 'sensor.camera.rgb', 'x':0.7, 'y':-0.4, 'z': 1.60, 'roll': 0.0, 'pitch': 0.0,
+                    'yaw': -45.0, 'width': 800, 'height': 600, 'fov': 100, 'id': 'Left'},
+                   {'type': 'sensor.camera.rgb', 'x':0.7, 'y':0.4, 'z':1.60, 'roll':0.0, 'pitch':0.0, 'yaw':45.0,
+                    'width':800, 'height':600, 'fov':100, 'id': 'Right'},
+                   {'type': 'sensor.lidar.ray_cast', 'x': 0.7, 'y': -0.4, 'z': 1.60, 'roll': 0.0, 'pitch': 0.0,
+                    'yaw': -45.0, 'id': 'LIDAR'},
+                   {'type': 'sensor.other.gnss', 'x': 0.7, 'y': -0.4, 'z': 1.60, 'id': 'GPS'},
+                   {'type': 'sensor.speedometer','reading_frequency': 25, 'id': 'speed'}
+
+                  ]
+          
+        """
+
 
         return sensors
 
