7b459d87a85166bc55246b587042def3ffa97191
felipecode <fcodevilla@cvc.uab.es>
2019-05-03 17:27:44 -0400

    adding processeds

--- tools/image_process.py
+++ tools/image_process.py
@@ -80,7 +80,7 @@ def reshape_images(image_type, image_name, out_path):
         scipy.misc.imsave(out_path, center)
 
     else:
-        raise ValueError("Unexpected Size")
+        print (" size not for processing")
 
 
 # ***** main loop *****
@@ -193,125 +193,9 @@ if __name__ == "__main__":
 
                     if out_name is not None:
                         batch_path_name = os.path.join(*(['/'] + out_name.split('/')[0:-1]))
-                        with open(os.path.join(batch_path_name, 'processed')) as f:
+                        with open(os.path.join(batch_path_name, 'processed'), 'w') as f:
                             pass
 
 
                     count_batch += 1
                 count_exp += 1
-
-    """
-    for episode in episodes_list[args.start_episode:]:
-        print ('Episode ', episode)
-        if 'episode' not in episode:
-            episode = 'episode_' + episode
-
-        if os.path.exists(os.path.join(episode, "checked")) or os.path.exists(
-                os.path.join(episode, "processed2")) \
-                or os.path.exists(os.path.join(episode, "bad_episode")):
-            # Episode was not checked. So we dont load it.
-            print(" This episode was already checked ")
-            continue
-        # Take all the measurements from a list
-        try:
-            measurements_list = glob.glob(os.path.join(episode, 'measurement*'))
-            sort_nicely(measurements_list)
-            print (" Purging other data")
-            print ("Lidar")
-            purge(episode, "Lidar*")
-
-            print (episode)
-            if args.delete_depth:
-                print ("***Depth***")
-                purge(episode, "CentralDepth*")
-                purge(episode, "LeftDepth*")
-                purge(episode, "RightDepth*")
-
-            if args.delete_semantic_segmentation:
-                print ("***Purging SemanticSeg***")
-                purge(episode, "CentralSemanticSeg*")
-                purge(episode, "LeftSemanticSeg*")
-                purge(episode, "RightSemanticSeg*")
-
-            bad_episode = False
-            if len(measurements_list) <= 1:
-                print (" Episode is empty")
-                purge(episode, '.')
-                bad_episode = True
-                continue
-
-            for measurement in measurements_list[:-3]:
-
-                data_point_number = measurement.split('_')[-1].split('.')[0]
-
-                with open(measurement) as f:
-                    measurement_data = json.load(f)
-
-                reshape_images("RGB", episode, data_point_number)
-                if not args.delete_depth:
-                    reshape_images("SemanticSeg", episode, data_point_number)
-
-                if not args.delete_depth:
-                    reshape_images("Depth", episode, data_point_number)
-
-                if 'forwardSpeed' in  measurement_data['playerMeasurements']:
-                    speed = measurement_data['playerMeasurements']['forwardSpeed']
-                else:
-                    speed = 0
-
-                float_dicts = {'steer': measurement_data['steer'],
-                               'throttle': measurement_data['throttle'],
-                               'brake': measurement_data['brake'],
-                               'speed_module': speed,
-                               'directions': measurement_data['directions'],
-                               "pedestrian": measurement_data['stop_pedestrian'],
-                               "traffic_lights": measurement_data['stop_traffic_lights'],
-                               "vehicle": measurement_data['stop_vehicle'],
-                               'angle': -30.0}
-
-
-            for measurement in measurements_list[-3:]:
-                data_point_number = measurement.split('_')[-1].split('.')[0]
-                purge(episode, "CentralRGB_" + data_point_number + '.png')
-                purge(episode, "LeftRGB_" + data_point_number + '.png')
-                purge(episode, "RightRGB_" + data_point_number + '.png')
-                purge(episode, "CentralSemanticSeg_" + data_point_number + '.png')
-                purge(episode, "LeftSemanticSeg_" + data_point_number + '.png')
-                purge(episode, "RightSemanticSeg_" + data_point_number + '.png')
-                purge(episode, "CentralDepth_" + data_point_number + '.png')
-                purge(episode, "LeftDepth_" + data_point_number + '.png')
-                purge(episode, "RightDepth_" + data_point_number + '.png')
-                os.remove(measurement)
-
-            if not bad_episode:
-                done_file = open(os.path.join(episode, "processed2"), 'w')
-                done_file.close()
-
-        except:
-            import traceback
-            traceback.print_exc()
-            print (" Error on processing")
-            done_file = open(os.path.join(episode, "bad"), 'w')
-            done_file.close()
-
-            continue
-
-
-
-
-        # The last one we delete
-        data_point_number = measurements_list[-1].split('_')[-1].split('.')[0]
-        print (data_point_number)
-        purge(episode, "CentralRGB_" + data_point_number + '.png')
-        purge(episode, "LeftRGB_" + data_point_number + '.png')
-        purge(episode, "RightRGB_" + data_point_number + '.png')
-
-
-
-if ss_center.shape[0] == 600:
-    ss_center = rgb_center[IMAGE_CUT[0]:IMAGE_CUT[1], ...]
-    ss_center = scipy.misc.imresize(rgb_center, (88, 200))
-    scipy.misc.imsave(os.path.join(episode, rgb_center_name), rgb_center)
-
-
-"""
